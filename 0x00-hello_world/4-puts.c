7ecade2e04f56d4aa9422cd099ad1c366fa07865
James Adakole <jambone.james82@gmail.com>
2022-04-07 20:04:52 +0100

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@ int main(void)
 	/**
 	 * main - prints the string "Programming is like building a multilingual puzzle"
 	 * Return: 0 if exited properly, non-zero otherwise
-	 * 
 	 * */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
